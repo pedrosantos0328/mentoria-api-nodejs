cf045b8a305d71900542164ced4b63caeb2fb69e
Pedro Cesar <pedroskr000@gmail.com>
2023-02-08 22:01:57 -0300

    criaçao da primeira rota

--- api/localApi/app.js
+++ api/localApi/app.js
@@ -4,5 +4,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-
+app.get("/primeira-rota", function(request,response){
+    return response.json("hellow word");
+});
 module.exports = app;
