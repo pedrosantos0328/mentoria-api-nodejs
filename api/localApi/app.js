ff86d6e6b06a4a61b1a24ffba90208598fc77c73
Your Name <nreis787@gmail.com>
2023-02-09 18:35:12 -0300

    criando rota de soma

--- api/localApi/app.js
+++ api/localApi/app.js
@@ -4,7 +4,14 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/primeira-rota", function(request,response){
-    return response.json("hellow word");
+app.get("/primeira-rota", function (request, response) {
+    return response.json("hello word");
+});
+
+app.get("/soma/:num1/:num2", function (request, response) {
+    const num1 = Number(request.params.num1);
+    const num2 = Number(request.params.num2);
+    const soma= num1+num2;
+    return response.json(soma);
 });
 module.exports = app;
